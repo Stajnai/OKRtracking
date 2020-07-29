7101e056c1a0aa7af50ff05cb065b15553f41027
Sophia Tajnai <Stajnai@carthage.edu>
2020-07-29 18:25:40 -0500

    Updated Output Video
    changed name to outputVideo, took away initial output handle. This still needs some fixing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import os.path
+from os import path
 
 #notes for sof - attricbutes are lowercase and methods are capital
 #A project is created for any video analysis and requires a valid filepath to create
@@ -13,7 +14,7 @@ class Project:
 	def __init__(self,inputFilePath):
 		
 		#The class attributes
-		self.video = cv2.VideoCapture(inputFilePath)
+		self.inputVideo = cv2.VideoCapture(inputFilePath)
 		self.inputFilePath = inputFilePath
 		self.outputFilePath = "OKRtracking.avi" #os.path.expanduser("~")+"//Downloads//"   <-- this is for downloads?
 		self.fps = 20.0
@@ -24,12 +25,10 @@ class Project:
 
 		#check the input file path by trying to read the first frame
 		try:
-			ret,self.frame = self.video.read()
+			ret,self.frame = self.inputVideo.read()
 			height, width, channels = self.frame.shape 
 			self.ratio = (width,height)
 			self.outputVideo = cv2.VideoWriter(self.outputFilePath, Project.fourcc, self.fps, self.ratio) #this is saying what our output file is
-
-
 		except:
 			self.inputFilePath = None
 			print("There was an error with the input file path.")
@@ -39,23 +38,28 @@ class Project:
 		#maybe only allow this if inputFilePath == None? can't change for project
 		try:
 			self.inputFilePath = inputFilePath
-			self.video = cv2.VideoCapture(inputFilePath)
-			ret,self.frame = self.video.read()
+			self.inputVideo = cv2.VideoCapture(inputFilePath)
+			ret,self.frame = self.inputVideo.read()
 			height, width, channels = self.frame.shape 
 			self.ratio = (width,height)
-			self.outputVideo = cv2.VideoWriter(self.outputFilePath, Project.fourcc, self.fps, self.ratio) #this is saying what our output file is
+			#maybe not do this here for orgnization?#self.outputVideo = cv2.VideoWriter(self.outputFilePath, Project.fourcc, self.fps, self.ratio) #this is saying what our output file is
 		except:
 			print("There was an error with the input file path.")
 
 	#sets the output file path and updates the output video codec function
 	def setOutputFilePath(self,outputFilePath):
 		self.outputFilePath = outputFilePath
-		self.outputVideo = cv2.VideoWriter(self.outputFilePath, Project.fourcc, self.fps, self.ratio) #this is saying what our output file is
+		#self.outputVideo = cv2.VideoWriter(self.outputFilePath, Project.fourcc, self.fps, self.ratio) #this is saying what our output file is
+		if path.exists(outputFilePath) == False:
+			self.outputFilePath = "OKRtracking.avi"
+			self.outputVideo = cv2.VideoWriter(self.outputFilePath, Project.fourcc, self.fps, self.ratio) #this is saying what our output file is
+			print("There was an error with the output file path")
+
 	
 	#returns true or false if a frame is read and update the self.frame attribute
 	def getFrame(self):
 		prevFrame = self.frame
-		ret, self.frame = self.video.read()
+		ret, self.frame = self.inputVideo.read()
 		return ret, self.frame #do i need the self.frame?
 
 
@@ -66,5 +70,15 @@ SofsProject.setInputFilePath("TestData//stillFish.mp4")
 if SofsProject.getFrame():
 	cv2.imshow("fish",SofsProject.frame)
 
+SofsProject.setOutputFilePath("testWrite.mp4") #for some reason it outputs two files if outputfil
+SofsProject.setOutputFilePath("testWriteCheck.mp4") #for some reason it outputs two files if outputfil
+
+#'''
+while SofsProject.getFrame()[0]:
+	SofsProject.outputVideo.write(SofsProject.frame)
+
+SofsProject.inputVideo.release()
+SofsProject.outputVideo.release()
+#'''
 cv2.waitKey(0)
 cv2.destroyAllWindows()
